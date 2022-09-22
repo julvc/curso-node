ab5d02ffa5b28466811f3444a786746db909cd9d
Julio Varas <jvarascontreras@gmail.com>
2022-09-22 20:58:59 -0300

    Se realiza conexion a BBDD Mongo

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config(); //* para decirle a la aplicacion que utilice variables de entorno 
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors()); //* nos permite evitar el error de origen cruzado, de momento todos los clientes se pueden conectar
+
+const port = process.env.PORT || 3001; //* en caso de que llegue a fallar hay un puesto alternativo para conectarse 
+
+//* pasamos como primer argumento el puerto y luego una funcion
+app.listen(port, ()=> {
+    //* alt+96 para las comillas francesas
+    console.log(`Servidor en linea : http://localhost:${port}`);
+});
---
+++ config/mongo.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const dbConnect = () => {
+    // const DB_URI = process.env.DB_URI;
+    const DB_URI = 'mongodb://localhost:27017/db_usuarios';
+    mongoose.connect(DB_URI, {
+        useNewUrlParse: true,
+        useUnifiedTopology: true,
+    }, (err,res) =>{
+        if (!err) {
+            //* si no hay error
+            console.log("*** CONEXION CORRECTA ***");
+        }else{
+            console.log("*** ERROR EN LA CONEXION ***");
+        }    
+    });
+};
+
+module.exports = dbConnect;
+
+//* useNewUrlParser: true, si no especifica mongoose utilizará un método obsoleto, para intentar parsear la uri de conexión.
+
+//* useUnifiedTopology: true para que intente la conexión cada milisegundo
