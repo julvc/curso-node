024e28f66dca1ac202bba16c318b2366312ed5ff
Julio Varas <jvarascontreras@gmail.com>
2022-09-26 19:49:41 -0300

    Creacion de rutas

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.use(cors()); //* nos permite evitar el error de origen cruzado, de momento t
 
 const port = process.env.PORT || 3001; //* en caso de que llegue a fallar hay un puesto alternativo para conectarse 
 
+//* Aqui vamos a cargar nuestras rutas
+//*http://locaclhost:3000/api/nombreDeLaRUta"
+//*http://localhost:3000/api
+/* app.use("/api", (req,res) => {
+    res.send({ saludo: 'Hola Ruta accedida'});
+}); */
+app.use("/api", require("./routes"));
 //* pasamos como primer argumento el puerto y luego una funcion
 app.listen(port, ()=> {
     //* alt+96 para las comillas francesas
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+
+const PATH_ROUTES = __dirname; //* Nos devuelve el path en donde esta ubicado este archivo "index.js" (RUTA ABSOLUTA)
+//console.log(PATH_ROUTES);
+//const a = fs.readdirSync(PATH_ROUTES); //* retorna un array
+//['index.js','users.js','storage.js']
+const removeExtension = (fileName) =>{
+    return fileName.split('.').shift();
+};
+
+fs.readdirSync(PATH_ROUTES).filter( (file)=> {
+    console.log(file); // index.js y users.js
+    const name = removeExtension(file);
+    if (name != 'index') {
+        router.use(`/${name}`, (req, res) => {
+            res.send({aviso: 'la ruta dinamica se creo'})
+        });
+    }
+})
+
+
+module.exports = router;
